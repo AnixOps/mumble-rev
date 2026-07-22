63d386782e6bca3fbfe6a6146571b7099585669e
kalijerry <kalijerryuk@gmail.com>
2026-07-23 05:54:21 +0800

    CI(client): Remove external database services

--- .github/actions/install-dependencies/install_macos_static_x86_64.sh
+++ .github/actions/install-dependencies/install_macos_static_x86_64.sh
@@ -10,17 +10,3 @@ verify_required_env_variables_set
 brew install coreutils aria2 gnu-tar xz
 
 make_build_env_available "tar.xz"
-
-
-# Setup PostgreSQL database for the Mumble tests
-# Note: we don't configure MySQL as that's not installed on the Azure runners for macOS
-# by default and installing it via homebrew takes forever.
-
-brew install postgresql
-brew link postgresql
-brew services start postgresql
-
-# Give the database some time to start
-sleep 5
-
-configure_database_tables "postgresql"
--- .github/actions/install-dependencies/install_ubuntu_static_x86_64.sh
+++ .github/actions/install-dependencies/install_ubuntu_static_x86_64.sh
@@ -22,21 +22,3 @@ sudo apt -y install \
 verify_required_env_variables_set
 
 make_build_env_available "tar.xz"
-
-# MySQL and PostgreSQL are pre-installed on GitHub-hosted runners.
-# Set them up for the Mumble tests
-
-echo -e "[mysqld]\nlog-bin-trust-function-creators = 1\nsocket=/var/run/mysqld/mysqld.sock\n\n" | sudo tee -a /etc/mysql/my.cnf
-
-# By default the statically compiled MySQL seems to ignore local config files
-# and instead search for a socket under /tmp/, which doesn't exist.
-# Hence, we use this environment variable to explicitly tell it the socket location
-echo "MYSQL_UNIX_PORT=/var/run/mysqld/mysqld.sock" >> "$GITHUB_ENV"
-
-sudo systemctl enable mysql.service
-sudo systemctl start mysql.service
-
-sudo systemctl enable postgresql.service
-sudo systemctl start postgresql.service
-
-configure_database_tables "mysql" "postgresql"
